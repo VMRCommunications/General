e8925365bfdcc119ed0f90df6281fb5f84122640
dreamerdad <hugh@vmrcommunications.com>
2024-05-02 11:35:03 -0400

    Update vmr-ch-gpt.js
    mapping

--- src/catholicherald/js/vmr-ch-gpt.js
+++ src/catholicherald/js/vmr-ch-gpt.js
@@ -3,6 +3,7 @@ googletag.cmd.push(function() {
   var mapping1 = googletag.sizeMapping()
                             .addSize([1000, 1000], [[728, 90]])
     .addSize([300, 100], [[320, 50]])
+    .addSize([0, 0], [])
                             .build();                         
   googletag.defineSlot('/4580722/CatholicHerald_4', [[728,90],[320,50]], 'div-gpt-ad-4206571-4')
     .defineSizeMapping(mapping1)
