c721add6431e16b838eb20cd05f3c2d94c616b27
dreamerdad <hugh@vmrcommunications.com>
2024-05-03 12:43:44 -0400

    Update vmr-ch-gpt.js
    3 to 5

--- src/catholicherald/vmr-ch-gpt.js
+++ src/catholicherald/vmr-ch-gpt.js
@@ -4,7 +4,7 @@ googletag.cmd.push(function() {
            .addService(googletag.pubads());
   googletag.defineSlot('/4580722/CatholicHerald_2', [[300,250],[160,600],[970,250],[336,280]], 'div-gpt-ad-4206571-2')
            .addService(googletag.pubads());
-  googletag.defineSlot('/4580722/CatholicHerald_3', [[300,250],[336,280],[160,600],[970,250]], 'div-gpt-ad-4206571-3')
+  googletag.defineSlot('/4580722/CatholicHerald_5', [[300,250],[336,280],[160,600],[970,250]], 'div-gpt-ad-4206571-3')
            .addService(googletag.pubads());
   googletag.defineSlot('/4580722/CatholicHerald_4', [[300,250],[336,280],[160,600],[970,250]], 'div-gpt-ad-4206571-4')
            .addService(googletag.pubads());
