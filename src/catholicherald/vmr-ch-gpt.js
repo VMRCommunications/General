ea8ecac95fa4e361904a01e6aeb55dd5e5f815d0
dreamerdad <hugh@vmrcommunications.com>
2024-05-03 12:37:25 -0400

    Update vmr-ch-gpt.js

--- src/catholicherald/vmr-ch-gpt.js
+++ src/catholicherald/vmr-ch-gpt.js
@@ -12,3 +12,20 @@ googletag.cmd.push(function() {
   googletag.pubads().enableSingleRequest();
   googletag.enableServices();
 });
+
+// code below to change margin and padding on top ad unit for mobile users so ad does not get cut off
+
+// Select the input element with class ".default--input"
+const defaultInput = document.querySelector('.default--input');
+
+// Apply padding using JavaScript
+defaultInput.style.padding = '10px 20px';
+
+// Check if the viewport width is less than or equal to 1050px
+if (window.innerWidth <= 1050) {
+    // Select the element with class ".header--bottom__mobile--search"
+    const mobileSearch = document.querySelector('.header--bottom__mobile--search');
+
+    // Apply margin using JavaScript
+    mobileSearch.style.marginTop = '5px';
+}
