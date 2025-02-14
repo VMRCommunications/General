677aa6440a8e8f7695245b220b9801d838e08152
dreamerdad <hugh@vmrcommunications.com>
2025-02-13 23:49:37 -0500

    Update vmr-ch-gpt.js

--- src/catholicherald/js/vmr-ch-gpt.js
+++ src/catholicherald/js/vmr-ch-gpt.js
@@ -2,13 +2,14 @@ window.googletag = window.googletag || {cmd: []};
 googletag.cmd.push(function() {
   var mapping1 = googletag.sizeMapping()
                             .addSize([1000, 0], [[728, 90]])
-    .addSize([303, 0], [[320, 50]])
+    .addSize([301, 0], [[320, 50]])
     .addSize([0, 0], [])
                             .build();                         
   googletag.defineSlot('/4580722/CatholicHerald_4', [[728,90],[320,50]], 'div-gpt-ad-4206571-4')
     .defineSizeMapping(mapping1)
            .addService(googletag.pubads());
-googletag.defineSlot('/4580722/CatholicHerald_5', [[728,90]], 'CH001-Top')
+/*
+  googletag.defineSlot('/4580722/CatholicHerald_5', [[728,90]], 'CH001-Top')
            .addService(googletag.pubads());
 googletag.defineSlot('/4580722/CatholicHerald_8', ['fluid',[300,250]], 'CH003')
            .addService(googletag.pubads()); 
@@ -28,6 +29,7 @@ googletag.defineSlot('/4580722/CatholicHerald_9', ['fluid',[300,250],[302,250]],
            .addService(googletag.pubads());
   googletag.defineSlot('/4580722/CatholicHerald_1', [[300,250],[160,600],[970,250],[336,280]], 'div-gpt-ad-4206571-1')
            .addService(googletag.pubads());
+           */
  googletag.pubads().enableSingleRequest(); 
   googletag.enableServices();
 });
